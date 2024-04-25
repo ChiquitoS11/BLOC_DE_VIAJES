fb13f4de9e9c0fb440ed9327627c96cf737d944c
Victor de la Fuente <160121458+victordelafu@users.noreply.github.com>
2024-04-25 12:50:59 +0200

    asdas
    asdas

--- clientes/src/main/java/com/ilerna/clientes/service/GestorUsuario.java
+++ clientes/src/main/java/com/ilerna/clientes/service/GestorUsuario.java
@@ -4,10 +4,38 @@
  */
 package com.ilerna.clientes.service;
 
+import com.ilerna.clientes.database.Conexion;
+import com.ilerna.clientes.entity.Fabricante;
+import com.ilerna.clientes.entity.Usuario;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Alumno
  */
 public class GestorUsuario {
+    Conexion c = new Conexion();
     
+    public List<Usuario> listar() throws SQLException {
+        Statement consulta = c.conectar().createStatement();
+        ResultSet rs = consulta.executeQuery("SELECT * FROM fabricante");
+        List<Usuario> usuarios = new ArrayList<>();
+
+        while (rs.next()) {
+            Usuario u = new Usuario(
+                    rs.getInt("id"),
+                    rs.getString("nombre"),
+                    rs.getString("apellidos")
+            );
+            usuarios.add(u);
+        }
+        rs.close();
+        consulta.close();
+        return usuarios;
+    }
 }
+
