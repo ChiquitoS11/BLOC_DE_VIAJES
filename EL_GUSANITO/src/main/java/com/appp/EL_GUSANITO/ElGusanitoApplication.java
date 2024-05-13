700939e8605a21426c20ae0ea99628b09a097ae5
ChiquitoS11 <142192480+ChiquitoS11@users.noreply.github.com>
2024-05-13 14:24:41 +0200

    PROYETO_SRINGBOOT
    -add springBoot desde cero

---
+++ EL_GUSANITO/src/main/java/com/appp/EL_GUSANITO/ElGusanitoApplication.java
@@ -0,0 +1,13 @@
+package com.appp.EL_GUSANITO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElGusanitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElGusanitoApplication.class, args);
+	}
+
+}
---
+++ EL_GUSANITO/src/test/java/com/appp/EL_GUSANITO/ElGusanitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appp.EL_GUSANITO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElGusanitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
