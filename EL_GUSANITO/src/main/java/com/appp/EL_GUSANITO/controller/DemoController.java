3dae6d781da8e651d356761dc5f4bf01446e619a
ChiquitoS11 <142192480+ChiquitoS11@users.noreply.github.com>
2024-05-14 05:19:44 +0200

    PERSONAL PAGE
    add new page

---
+++ EL_GUSANITO/src/main/java/com/appp/EL_GUSANITO/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.appp.EL_GUSANITO.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author ChiquitoS11
+ */
+@Controller
+public class DemoController {
+    	@GetMapping("/saludar")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
