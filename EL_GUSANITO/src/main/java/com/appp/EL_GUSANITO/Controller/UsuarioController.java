559abe3a084800a4411deecc1be4051d32d669ec
ChiquitoS11 <142192480+ChiquitoS11@users.noreply.github.com>
2024-05-16 14:36:29 +0200

    need fix again

--- EL_GUSANITO/src/main/java/com/appp/EL_GUSANITO/Controller/UsuarioController.java
+++ EL_GUSANITO/src/main/java/com/appp/EL_GUSANITO/Controller/UsuarioController.java
@@ -66,6 +66,7 @@ public class UsuarioController {
                 return "redirect:/login-register-loginbad";
             }
 
-            return "redirect:/principal/inicio?usuarioActual=" + nombreUsuario;
+            return "redirect:/principal/inicio";
+//            return "redirect:/principal/inicio?usuarioActual=" + nombreUsuario;
         }
 }
